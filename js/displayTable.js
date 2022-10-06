a71a380915eaa381df5c9d7e8ac4fd760db8263e
Chaplin16 <chaplin16@orange.fr>
2022-10-06 21:58:35 +0200

    add function displayTable.js

--- js/displayTable.js
+++ js/displayTable.js
@@ -2,20 +2,29 @@ const painting = [
     {
         id: 1,
         h3: "Soutenez Le musée",
-        text: "Appel aux dons",
-        img:"images/musee-giverny-exposition-tableaux.png"
+        figcaption: "Appel aux dons",
+        img:"images/musee-giverny-exposition-tableaux.png",
+        alt:"Intérieur du musée de Giverny, exposition de tableaux"
     },
     {
         id: 2,
         h3: "Au coeur du musée",
-        text: "Boutique / Librairie",
-        img:"images/musee-giverny-boutique-librairie.png"
-    },
+        figcaption: "Boutique / Librairie",
+        img:"images/musee-giverny-boutique-librairie.png",
+        alt:"Librairie et boutique dans le musée de Giverny"
+    }
 ]
 
 function displayPainting() {
     const paintingMuseum = document.querySelector('#paintingMuseum');
     painting.forEach(function(e){
-        paintingMuseum.innerHTML += ``
+        paintingMuseum.innerHTML += `<figure>
+                                        <img src="${e.img}"
+                                            alt="${e.alt}">
+                                            <h3>${e.h3}</h3>
+                                        <figcaption>${e.figcaption}</figcaption>
+                                    </figure>`
     })
 }
+
+displayPainting();
