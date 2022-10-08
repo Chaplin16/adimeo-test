1b80ed65e469a803c39870e146d7dd570c2b5d1d
Chaplin16 <chaplin16@orange.fr>
2022-10-08 18:56:31 +0200

    function du logo menu en hover

---
+++ js/displayHover.js
@@ -0,0 +1,11 @@
+const menuLogo = document.querySelector('.menu__logo');
+
+
+
+document.getElementById('menu__hamburger').addEventListener('click', function() {
+        menuLogo.classList.add('filter');
+        menuLogo.style.filter = "invert(46%) sepia(31%) saturate(6374%) hue-rotate(187deg) brightness(107%) contrast(88%)"
+    })
+
+
+ 
