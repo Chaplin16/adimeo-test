29757a1226347779ae7d5d8a71db6049d8379c7c
Chaplin16 <chaplin16@orange.fr>
2022-10-08 22:37:07 +0200

    link to # in menu and border bottom on hover

--- js/displayHover.js
+++ js/displayHover.js
@@ -1,11 +1,14 @@
+const menuTitle = document.querySelectorAll('.menu__title');
 const menuLogo = document.querySelector('.menu__logo');
+let timeOut;
 
-
-
-document.getElementById('menu__hamburger').addEventListener('click', function() {
+menuTitle.forEach(menu => {
+    menu.addEventListener('click', function(displayHover) {
         menuLogo.classList.add('filter');
         menuLogo.style.filter = "invert(46%) sepia(31%) saturate(6374%) hue-rotate(187deg) brightness(107%) contrast(88%)"
+        timeOut = setTimeout(displayHover, 0);
+        clearTimeout(timeOut, 2000)
     })
-
+})
 
  
