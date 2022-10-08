42198178f4f96bb2f5a01dab4d46451c0eb124df
Chaplin16 <chaplin16@orange.fr>
2022-10-09 00:01:01 +0200

    function changeBgLogo for the menu ok

--- js/displayHover.js
+++ js/displayHover.js
@@ -3,11 +3,11 @@ const menuLogo = document.querySelector('.menu__logo');
 let timeOut;
 
 menuTitle.forEach(menu => {
-    menu.addEventListener('click', function(displayHover) {
-        menuLogo.classList.add('filter');
-        menuLogo.style.filter = "invert(46%) sepia(31%) saturate(6374%) hue-rotate(187deg) brightness(107%) contrast(88%)"
-        timeOut = setTimeout(displayHover, 0);
-        clearTimeout(timeOut, 2000)
+    menu.addEventListener('click', function() {
+        menuLogo.classList.add('bgLogo');
+        menuLogo.addEventListener("animationend", function () {
+            menuLogo.classList.remove("bgLogo");
+        })
     })
 })
 
